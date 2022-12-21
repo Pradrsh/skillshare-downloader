48fb5ef04bd9e611283a154b810cf8a0ae66952d
iamsumankarki <39690307+iamsumankarki@users.noreply.github.com>
2022-12-21 20:00:48 +0545

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,12 @@
 from downloader import Downloader
 
-cookie = """
-ADD YOUR COOKIE HERE
-"""
+# use cookie manager and find PHPSESSID and replace xxxx only
+cookie = "PHPSESSID=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 dl = Downloader(cookie=cookie)
 
 # download by class URL:
-dl.download_course_by_url('https://www.skillshare.com/classes/Art-Fundamentals-in-One-Hour/189505397')
+dl.download_course_by_url('https://www.skillshare.com/en/classes/Art-Fundamentals-in-One-Hour/189505397')
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
