28771deae3af6cb93dee177c89ecfaa8752312b2
iamsumankarki <39690307+iamsumankarki@users.noreply.github.com>
2022-12-21 20:02:59 +0545

    Update example.py

--- example.py
+++ example.py
@@ -7,6 +7,3 @@ dl = Downloader(cookie=cookie)
 
 # download by class URL:
 dl.download_course_by_url('https://www.skillshare.com/en/classes/Art-Fundamentals-in-One-Hour/189505397')
-
-# or by class ID:
-# dl.download_course_by_class_id(189505397)
